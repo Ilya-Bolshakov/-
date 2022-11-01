7ee0b77e0b74258980fac038c52c46a888f069d6
Илья <gamenest926@gmail.com>
2022-11-01 21:27:12 +0300

    Сделанное первое задание

--- Lab2/src/Main.java
+++ Lab2/src/Main.java
@@ -1,5 +1,28 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        ArrayList<Integer> firstList = new ArrayList<>();
+
+        firstList.add(3);
+        firstList.add(6);
+        firstList.add(3);
+
+
+        ArrayList<Integer> secondList = new ArrayList<>();
+
+        secondList.add(4);
+        secondList.add(2);
+        secondList.add(4);
+
+
+
+        var polynomialAdder = new PolynomialAdder(firstList, secondList); // 12 30 36 30 12
+        var result = polynomialAdder.addPolynomial();
+        for (var item : result) {
+            System.out.println(item);
+        }
     }
 }
--- Lab2/src/PolynomialAdder.java
+++ Lab2/src/PolynomialAdder.java
@@ -1,2 +1,32 @@
+import java.util.ArrayList;
+
 public class PolynomialAdder {
+    private ArrayList<Integer> firstPolynomial;
+    private ArrayList<Integer> secondPolynomial;
+
+
+    public PolynomialAdder(ArrayList<Integer> firstPolynomial, ArrayList<Integer> secondPolynomial) {
+        this.firstPolynomial = firstPolynomial;
+        this.secondPolynomial = secondPolynomial;
+    }
+
+    public ArrayList<Integer> addPolynomial() {
+        var maxDegree = firstPolynomial.size() + firstPolynomial.size() - 1;
+        var newPolynomial = new ArrayList<Integer>();
+        for (int i = 0; i < maxDegree; i++) {
+            newPolynomial.add(0);
+        }
+        int currentFirstPolynomialCoefficient = -1;
+        for (var first: firstPolynomial) {
+            currentFirstPolynomialCoefficient++;
+            int currentSecondPolynomialCoefficient = -1;
+            for (var second: secondPolynomial) {
+                currentSecondPolynomialCoefficient++;
+                int currentCoefficient = currentFirstPolynomialCoefficient + currentSecondPolynomialCoefficient;
+                var currentValue = newPolynomial.get(currentCoefficient);
+                newPolynomial.set(currentCoefficient, currentValue + (first * second));
+            }
+        }
+        return newPolynomial;
+    }
 }
