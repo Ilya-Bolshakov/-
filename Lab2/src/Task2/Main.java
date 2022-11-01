f676139b3859af0195125dcbdd398579faa0e5af
Илья <gamenest926@gmail.com>
2022-11-02 01:19:35 +0300

    Доделанное второе задание

--- Lab2/src/Task2/Main.java
+++ Lab2/src/Task2/Main.java
@@ -1,7 +1,15 @@
 package Task2;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.util.ArrayList;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        var path = "C:\\Users\\Ilya\\Desktop\\t.txt";
+
+        var objectNamesWorker = new ObjectNamesWorker(path);
+        var a = objectNamesWorker.generateSortedMap();
+        var b = objectNamesWorker.squeezeMap();
     }
 }
--- Lab2/src/Task2/ObjectNamesWorker.java
+++ Lab2/src/Task2/ObjectNamesWorker.java
@@ -1,5 +1,57 @@
 package Task2;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
 public class ObjectNamesWorker {
-    
+    TreeMap<UUID, String> treeMap = new TreeMap<>();
+    Collection<String> items;
+    String path;
+    public ObjectNamesWorker(String path) throws IOException {
+        super();
+        this.path = path;
+        generateCollectionFromFile();
+    }
+
+    private void generateCollectionFromFile() throws IOException {
+        this.items = new ArrayList<>();
+        FileReader fr = new FileReader(path);
+        Scanner scan = new Scanner(fr);
+        while (scan.hasNextLine()) {
+            items.add(scan.nextLine());
+        }
+        fr.close();
+    }
+
+    public ObjectNamesWorker(Collection<String> collection) {
+        super();
+        this.items = collection;
+    }
+
+    public TreeMap<UUID, String> getSortedMap() {
+        return treeMap;
+    }
+
+    public TreeMap<UUID, String> generateSortedMap() {
+        for (var item : items) {
+            treeMap.put(UUID.randomUUID(), item);
+        }
+        return getSortedMap();
+    }
+
+    public TreeMap<UUID, String> squeezeMap() {
+        var tempHashMap = new HashMap<String, UUID>();
+        var entrySet = treeMap.entrySet();
+        for (var item: entrySet) {
+            tempHashMap.putIfAbsent(item.getValue(), item.getKey());
+        }
+        var resultEntrySet = tempHashMap.entrySet();
+        var resultTreeMap = new TreeMap<UUID, String>();
+        for (var item: resultEntrySet) {
+            resultTreeMap.put(item.getValue(), item.getKey());
+        }
+        return resultTreeMap;
+    }
 }
