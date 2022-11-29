fec04618f18653c072a731a6c835e26cde6cce70
Илья <gamenest926@gmail.com>
2022-11-29 17:32:53 +0300

    Редактирование призера

--- Lab3/src/main/java/com/example/Lab3/AwarderController.java
+++ Lab3/src/main/java/com/example/Lab3/AwarderController.java
@@ -1,13 +1,11 @@
 package com.example.Lab3;
 
+import com.example.Lab3.Entity.Awarder;
 import com.example.Lab3.repos.AwarderRepos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class AwarderController {
@@ -23,11 +21,36 @@ public class AwarderController {
         return "awarder";
     }
 
-    @RequestMapping(value = "/awarder/addAwarder")
+    @RequestMapping(value = "/awarder/addAwarder", method = RequestMethod.GET)
     public String addAwarder(
             Model model) {
-        //model.addAttribute("name", name);
+        model.addAttribute("awarder", new Awarder());
         //var awarders = awarderRepos.findAll();
         return "add-awarder";
+    }
+
+    @RequestMapping(value = "/awarder/addAwarder", method = RequestMethod.POST)
+    public String addAwarderSubmit(@ModelAttribute Awarder awarder,
+            Model model) {
+        awarderRepos.save(awarder);
+        return "redirect:/awarder";
+    }
+
+    @RequestMapping(value = "/awarder/editAwarder/{id}", method = RequestMethod.GET)
+    public String editAwarder(@PathVariable(value = "id") Long id,
+            Model model) {
+        Awarder awarder = awarderRepos.findById(id).get();
+        model.addAttribute("awarder", awarder);
+        //var awarders = awarderRepos.findAll();
+        return "edit-awarder";
+    }
+
+    @RequestMapping(value = "/awarder/editAwarder", method = RequestMethod.POST)
+    public String editAwarderSubmit(@ModelAttribute Awarder awarder,
+                                   Model model) {
+        var item = awarderRepos.findById((long) awarder.getId()).get();
+        item = awarder;
+        awarderRepos.save(item);
+        return "redirect:/awarder";
     }
 }
